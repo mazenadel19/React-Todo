805d024efe077dc7a52f4938aeb4c43c56c1955e
Mazen <mazenadel19@gmail.com>
2021-07-10 18:24:20 +0200

    adding custom hook to replace get req

--- src/App.js
+++ src/App.js
@@ -1,59 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from './components/Tasks/Tasks'
+import NewTask from './components/NewTask/NewTask'
+import useHttp from './hooks/use-http'
 
-function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [tasks, setTasks] = useState([]);
-
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-				'https://react-dummy-movies-bf735-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-			)
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
-  return (
-    <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-    </React.Fragment>
-  );
+const reqConfig = {
+	url: 'https://react-dummy-movies-bf735-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
 }
 
-export default App;
+function App() {
+	const [tasks, setTasks] = useState([])
+
+	const transformedData = tasksObj => {
+		const loadedTasks = []
+
+		for (const taskKey in tasksObj) {
+			loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text })
+		}
+
+		setTasks(loadedTasks)
+	}
+
+	const {
+		isLoading,
+		error,
+		sendReqest: fetchTasks,
+	} = useHttp(reqConfig, transformedData)
+
+	useEffect(() => {
+		fetchTasks()
+	}, [])
+
+	const taskAddHandler = task => {
+		setTasks(prevTasks => prevTasks.concat(task))
+	}
+
+	return (
+		<React.Fragment>
+			<NewTask onAddTask={taskAddHandler} />
+			<Tasks
+				items={tasks}
+				loading={isLoading}
+				error={error}
+				onFetch={fetchTasks}
+			/>
+		</React.Fragment>
+	)
+}
+
+export default App
